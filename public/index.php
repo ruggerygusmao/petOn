959eea0174adc40c90d76108264020c0fa5990f3
DouglasGusmao <ruggery402@gmail.com>
2020-10-27 10:35:00 -0300

    correcao no index

--- public/index.php
+++ public/index.php
@@ -5,21 +5,12 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     
     <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="../assets/materialize/css/materialize.min.css"  media="screen,projection"/>
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta charset="UTF-8">
-<<<<<<< HEAD
-    <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link rel="stylesheet" href="../assets/materialize/css/materialize.min.css" media="screen,projection">
-      <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
-      
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-=======
->>>>>>> df024a074cd725e389947d3a768451876f340127
     <title>|| Projeto petOn ||</title>
 </head>
 <body class="grey darken-3">
@@ -55,9 +46,5 @@
   
   <!-- Compiled and minified JavaScript -->
   <script src="../assets/materialize/js/materialize.min.js"></script>
-  <script>
-    $(document).ready(function() {
-    M.updateTextFields();
-  });</script>
 </body>
 </html>
